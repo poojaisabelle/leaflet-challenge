942baa0e0043a63b9faef32d880f581cac76b99f
Pooja <pooja.isabelle@gmail.com>
2020-10-05 23:35:36 +1100

    Added popup message and got the color scheme to work

--- Leaflet Step 1/static/js/logic.js
+++ Leaflet Step 1/static/js/logic.js
@@ -1,12 +1,10 @@
-
-
-// Creating map object
+// Creating map object and passing in US coordinates 
 var myMap = L.map("map", {
     center: [37.0902, -95.7129],
-    zoom: 8
+    zoom: 5
 });
 
-// Adding the tile layer for the background of the map 
+// Adding the (light) tile layer for the background of the map 
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
@@ -14,13 +12,27 @@ L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?acce
     accessToken: API_KEY
 }).addTo(myMap);
 
+
 // Store the API in a queryURL 
 var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // Perform a GET request to the query URL 
 d3.json(queryURL, function(data) {
 
-    // Get the features data 
+
+    // Create function to obtain different colors according to magnitude of the earthquake 
+    // Will be used as fillColor when circles are added to the map
+    function getColor(d) {
+        return d >= 5 ? '#54278f' :
+               d >= 4 ? '#756bb1' :
+               d >= 3 ? '#9e9ac8' :
+               d >= 2 ? '#bcbddc' :
+               d >= 1 ? '#dadaeb' :
+                        '#f2f0f7' ;
+        
+    }
+
+    // Get the features data that contains necessary info 
     var features = data.features;
 
     // Loop through the features array and create the circles 
@@ -32,28 +44,20 @@ d3.json(queryURL, function(data) {
         var coords = features[i].geometry.coordinates;
 
         // Create circles
-        L.circle([coords[0], coords[1]], {
-            fillOpacity: 0.75,
-            color:"white",
-
-            // for the radius, scale the magnitudes 
-            // otherwise the circles will be too small
-            radius: magnitude *10000
-        }).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>")
+        L.circle(
+            [coords[1], coords[0]], {
+                fillOpacity: 0.75,
+                fillColor: getColor(magnitude),
+                color: "black",
+                weight: 0.25,
+                // for the radius, scale the magnitudes 
+                // otherwise the circles will be too small
+                radius: magnitude * 10000
+        }).bindPopup("<h2>" + features[i].properties.place + "</h2><hr><h4>" + new Date(features[i].properties.time) + "<br>" + 
+            "Location: [" + coords[1] + "," + coords[1] + "]" + "</h4>").addTo(myMap);
 
     }
 
-    // Create function to obtain different colors according to magnitude
-    // of the earthquake 
-    function getColor(d) {
-        return d >= 5 ? '#54278f' :
-               d >= 4 ? '#756bb1' :
-               d >= 3 ? '#9e9ac8' :
-               d >= 2 ? '#bcbddc' :
-               d >= 1 ? '#dadaeb' :
-                        '#f2f0f7' ;
-        
-    }
 
     // Set up the legend 
     var legend = L.control({position: "bottomright"});
