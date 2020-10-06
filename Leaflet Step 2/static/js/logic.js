dee2fbdfea661249602d2422c86b29a33e95e06c
Pooja <pooja.isabelle@gmail.com>
2020-10-06 15:26:02 +1100

    Added the circle markers to the earthquake layer

--- Leaflet Step 2/static/js/logic.js
+++ Leaflet Step 2/static/js/logic.js
@@ -86,65 +86,67 @@ d3.json(tectonicURL, function(data) {
     }
 });
 
+// Perform a GET request to the earthquake URL 
+d3.json(earthquakeURL, function(data) {
 
 
-    // // Create function to obtain different colors according to magnitude of the earthquake 
-    // // Will be used as fillColor when circles are added to the map
-    // function getColor(d) {
-    //     return d >= 5 ? '#54278f' :
-    //            d >= 4 ? '#756bb1' :
-    //            d >= 3 ? '#9e9ac8' :
-    //            d >= 2 ? '#bcbddc' :
-    //            d >= 1 ? '#dadaeb' :
-    //                     '#f2f0f7' ;
+    // Create function to obtain different colors according to magnitude of the earthquake 
+    // Will be used as fillColor when circles are added to the map
+    function getColor(d) {
+        return d >= 5 ? '#54278f' :
+               d >= 4 ? '#756bb1' :
+               d >= 3 ? '#9e9ac8' :
+               d >= 2 ? '#bcbddc' :
+               d >= 1 ? '#dadaeb' :
+                        '#f2f0f7' ;
         
-    // }
+    }
 
-    // // Get the features data that contains necessary info 
-    // var features = data.features;
+    // Get the features data that contains necessary info 
+    var features = data.features;
 
-    // // Loop through the features array and create the circles 
-    // for (var index = 0; index < features.length; index++) {
+    // Loop through the features array and create the circles 
+    for (var index = 0; index < features.length; index++) {
         
-    //     // Create new variables for the coordinates and magnitude 
-    //     // of the earthquakes 
-    //     var magnitude = features[index].properties.mag;
-    //     var coords = features[index].geometry.coordinates;
+        // Create new variables for the coordinates and magnitude 
+        // of the earthquakes 
+        var magnitude = features[index].properties.mag;
+        var coords = features[index].geometry.coordinates;
 
-    //     // Create circles and add them to the map 
-    //     L.circle([coords[1], coords[0]], {
-    //         fillOpacity: 0.75,
-    //         fillColor: getColor(magnitude),
-    //         color: "black",
-    //         weight: 0.5,
-
-    //         // for the radius, scale the magnitudes otherwise the circles will be too small
-    //         radius: magnitude * 17000
+        // Create circles and add them to the map 
+        var markers = L.circle([coords[1], coords[0]], {
+            fillOpacity: 0.75,
+            fillColor: getColor(magnitude),
+            color: "black",
+            weight: 0.5,
+            // for the radius, scale the magnitudes otherwise the circles will be too small
+            radius: magnitude * 17000
+        });
         
-    //     // Add a popup message when circle is clicked 
-    //     }).bindPopup("<h2>" + features[index].properties.place + "</h2><hr><h4>" + "Magnitude Level: " + magnitude + 
-    //         "<br>" + new Date(features[index].properties.time) + "<br>" + 
-    //         "Location: [" + coords[1] + ", " + coords[1] + "]" + "</h4>").addTo(myMap);
+        // Add to the earthquake layer and bind a popup message when circle is clicked 
+        markers.bindPopup("<h2>" + features[index].properties.place + "</h2><hr><h4>" + "Magnitude Level: " + magnitude + 
+            "<br>" + new Date(features[index].properties.time) + "<br>" + 
+            "Location: [" + coords[1] + ", " + coords[1] + "]" + "</h4>").addTo(earthquakes);
+    }
 
-    // }
+    // Set up the legend 
+    var legend = L.control({position: "bottomright"});
+    legend.onAdd = function() {
 
-    // // Set up the legend 
-    // var legend = L.control({position: "bottomright"});
-    // legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend"),
+            magLevels = [0, 1, 2, 3, 4, 5],
+            labels = [];
 
-    //     var div = L.DomUtil.create("div", "info legend"),
-    //         magLevels = [0, 1, 2, 3, 4, 5],
-    //         labels = [];
+        // Loop through the various magnitudes and assign the colors 
+        for (var i = 0; i < magLevels.length; i++) {
 
-    //     // Loop through the various magnitudes and assign the colors 
-    //     for (var i = 0; i < magLevels.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(magLevels[i] + 1) + '"></i> ' +
+                magLevels[i] + (magLevels[i + 1] ? '&ndash;' + magLevels[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
 
-    //         div.innerHTML +=
-    //             '<i style="background:' + getColor(magLevels[i] + 1) + '"></i> ' +
-    //             magLevels[i] + (magLevels[i + 1] ? '&ndash;' + magLevels[i + 1] + '<br>' : '+');
-    //     }
-    //     return div;
-    // };
-
-    // legend.addTo(myMap)
+    legend.addTo(myMap)
+});
 
